d080df218007b350bdd662d289dcb9f0c614aebc
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2025-01-15 07:38:49 +0000

    Refactor training loop to improve prediction handling and dynamic learning rate adjustment

--- grad.c
+++ grad.c
@@ -1,6 +1,8 @@
 #include <time.h>
 #include "grad.h"
 
+#define N_EPOCHS 20
+
 double compute_loss(double** pred, double** tgt, int n, int m) {
     double l = 0;
     for(int i = 0; i < n; i++)
@@ -9,6 +11,21 @@ double compute_loss(double** pred, double** tgt, int n, int m) {
             l += d * d;
         }
     return l / (n * m);
+}
+
+double** get_predictions(Net* net, Data* data, double** act) {
+    double** pred = malloc(data->n * sizeof(double*));
+    for(int i = 0; i < data->n; i++) {
+        pred[i] = malloc(data->fy * sizeof(double));
+        fwd(net, data->X[i], act);
+        memcpy(pred[i], act[4], data->fy * sizeof(double));
+    }
+    return pred;
+}
+
+void free_predictions(double** pred, int n) {
+    for(int i = 0; i < n; i++) free(pred[i]);
+    free(pred);
 }
 
 int main() {
@@ -39,37 +56,33 @@ int main() {
     }
     
     double best_loss = INFINITY, prev_loss = INFINITY;
-    for(int e = 0; e < 10; e++) {
-        double** pred = malloc(data->n * sizeof(double*));
-        double curr_loss = 0;
-        
+    for(int e = 0; e < N_EPOCHS; e++) {
+        // Training pass
         for(int i = 0; i < data->n; i++) {
-            pred[i] = malloc(data->fy * sizeof(double));
             fwd(net, data->X[i], act);
-            memcpy(pred[i], act[4], data->fy * sizeof(double));
             
             for(int j = 0; j < data->fy; j++) {
                 double diff = act[4][j] - data->y[i][j];
                 grad[4][j] = 2 * diff;
-                curr_loss += diff * diff;
             }
-            
-            if(curr_loss > prev_loss) net->lr *= 0.95;
-            else net->lr *= 1.05;
-            net->lr = fmax(1e-9, fmin(0.01, net->lr));
             
             bwd(net, act, grad);
         }
         
+        // Evaluate loss after training pass
+        double** pred = get_predictions(net, data, act);
         double loss = compute_loss(pred, data->y, data->n, data->fy);
+        free_predictions(pred, data->n);
+        
+        if(loss > prev_loss) net->lr *= 0.95;
+        else net->lr *= 1.05;
+        net->lr = fmax(1e-9, fmin(0.01, net->lr));
+        
         if(loss < best_loss) best_loss = loss;
         prev_loss = loss;
         
-        if(e % 2 == 0 || e == 9)
+        if(e % 2 == 0 || e == N_EPOCHS-1)
             printf("%-6d %.6f%s %.6e\n", e+1, loss, loss == best_loss ? " *" : "  ", net->lr);
-        
-        for(int i = 0; i < data->n; i++) free(pred[i]);
-        free(pred);
     }
     
     save_weights(weights_file, net);
@@ -80,17 +93,10 @@ int main() {
     net = load_weights(weights_file);
     data = load_csv(data_file);
     
-    double** pred = malloc(data->n * sizeof(double*));
-    for(int i = 0; i < data->n; i++) {
-        pred[i] = malloc(data->fy * sizeof(double));
-        fwd(net, data->X[i], act);
-        memcpy(pred[i], act[4], data->fy * sizeof(double));
-    }
-    
+    double** pred = get_predictions(net, data, act);
     printf("Loaded weights loss: %.6f\n", compute_loss(pred, data->y, data->n, data->fy));
     
-    for(int i = 0; i < data->n; i++) free(pred[i]);
-    free(pred);
+    free_predictions(pred, data->n);
     for(int i = 0; i < 5; i++) { free(act[i]); free(grad[i]); }
     free(act); free(grad);
     free_net(net);
